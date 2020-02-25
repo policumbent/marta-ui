facb586aff0527ffaad04e742eab13b485a4e134
Gabriele <gabriele.belluardo@outlook.it>
2020-02-25 17:08:14 +0100

    formatted

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -32,6 +32,8 @@ const Dashboard = () => {
   const [data, setData] = useState()
   const [history, setHistory] = useState()
   const [weather, setWeather] = useState()
+  const loading =
+    data === undefined || weather === undefined || history === undefined
 
   const updateHistory = useCallback(history => {
     let param = ['heartrate', 'cadence', 'power', 'speed']
@@ -71,15 +73,18 @@ const Dashboard = () => {
     setHistory(newHistory)
   }, [])
 
-  const updateData = useCallback(data => {
-    setData(data)
+  const updateData = useCallback(
+    data => {
+      setData(data)
 
-    if (isMounted.current) {
-      setTimeout(() => {
-        SocketIoHelper.requestData()
-      }, 300)
-    }
-  }, [isMounted])
+      if (isMounted.current) {
+        setTimeout(() => {
+          SocketIoHelper.requestData()
+        }, 300)
+      }
+    },
+    [isMounted]
+  )
 
   useEffect(() => {
     SocketIoHelper.requestData()
@@ -87,106 +92,102 @@ const Dashboard = () => {
 
     SocketIoHelper.getData(data => updateData(data))
     SocketIoHelper.getWeather(weather => setWeather(weather))
-
   }, [updateHistory, updateData])
 
-  const loading = () => (
+  const Loading = () => (
     <div className="animated fadeIn pt-1 text-center">Loading...</div>
   )
 
-  let view =
-    data === undefined || weather === undefined || history === undefined
-
-  return view ? (
-    loading
+  return loading ? (
+    Loading
   ) : (
-      <article>
-        <Row>
-          <Col xs="12" sm="6" lg="3">
-            <Card className="text-white bg-info">
-              <CardBody className="pb-0">
-                <ButtonGroup id="card1" className="float-right">
-                  <FaSpaceShuttle size={'1.5em'} />
-                </ButtonGroup>
-                <div className="text-value">{data.power}</div>
-                <div>Power [W]</div>
-              </CardBody>
-              <div className="chart-wrapper" style={{ height: '60px' }}>
-                <PowerCard data={data} history={history.miniChart} />
+    <article>
+      <Row>
+        <Col xs="12" sm="6" lg="3">
+          <Card className="text-white bg-info">
+            <CardBody className="pb-0">
+              <ButtonGroup id="card1" className="float-right">
+                <FaSpaceShuttle size={'1.5em'} />
+              </ButtonGroup>
+              <div className="text-value">{data.power}</div>
+              <div>Power [W]</div>
+            </CardBody>
+            <div className="chart-wrapper" style={{ height: '60px' }}>
+              <PowerCard data={data} history={history.miniChart} />
+            </div>
+          </Card>
+        </Col>
+
+        <Col xs="12" sm="6" lg="3">
+          <Card className="text-white bg-success">
+            <CardBody className="pb-0">
+              <ButtonGroup id="card2" className="float-right">
+                <GiCartwheel size={'1.5em'} />
+              </ButtonGroup>
+              <div className="text-value">{data.cadence}</div>
+              <div>Cadence [rpm]</div>
+            </CardBody>
+            <div className="chart-wrapper" style={{ height: '60px' }}>
+              <CadenceCard data={data} history={history.miniChart} />
+            </div>
+          </Card>
+        </Col>
+
+        <Col xs="12" sm="6" lg="3">
+          <Card className="text-white bg-warning">
+            <CardBody className="pb-0">
+              <ButtonGroup id="card3" className="float-right">
+                <GiSpeedometer size={'1.5em'} />
+              </ButtonGroup>
+              <div className="text-value">{data.speed}</div>
+              <div>Speed [km/h]</div>
+            </CardBody>
+            <div className="chart-wrapper" style={{ height: '60px' }}>
+              <SpeedCard data={data} history={history.miniChart} />
+            </div>
+          </Card>
+        </Col>
+
+        <Col xs="12" sm="6" lg="3">
+          <Card className="text-white bg-danger">
+            <CardBody className="pb-0">
+              <ButtonGroup id="card4" className="float-right">
+                <FiActivity size={'1.5em'} />
+              </ButtonGroup>
+              <div className="text-value">{data.heartrate}</div>
+              <div>Heartrate [bpm]</div>
+            </CardBody>
+            <div className="chart-wrapper" style={{ height: '60px' }}>
+              <HRCard data={data} history={history.miniChart} />
+            </div>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Card>
+            <CardBody>
+              <div
+                className="chart-wrapper"
+                style={{ height: `370px`, marginTop: 0 }}
+              >
+                <MainChart data={data} history={history.chart} />
               </div>
-            </Card>
-          </Col>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
 
-          <Col xs="12" sm="6" lg="3">
-            <Card className="text-white bg-success">
-              <CardBody className="pb-0">
-                <ButtonGroup id="card2" className="float-right">
-                  <GiCartwheel size={'1.5em'} />
-                </ButtonGroup>
-                <div className="text-value">{data.cadence}</div>
-                <div>Cadence [rpm]</div>
-              </CardBody>
-              <div className="chart-wrapper" style={{ height: '60px' }}>
-                <CadenceCard data={data} history={history.miniChart} />
-              </div>
-            </Card>
-          </Col>
-
-          <Col xs="12" sm="6" lg="3">
-            <Card className="text-white bg-warning">
-              <CardBody className="pb-0">
-                <ButtonGroup id="card3" className="float-right">
-                  <GiSpeedometer size={'1.5em'} />
-                </ButtonGroup>
-                <div className="text-value">{data.speed}</div>
-                <div>Speed [km/h]</div>
-              </CardBody>
-              <div className="chart-wrapper" style={{ height: '60px' }}>
-                <SpeedCard data={data} history={history.miniChart} />
-              </div>
-            </Card>
-          </Col>
-
-          <Col xs="12" sm="6" lg="3">
-            <Card className="text-white bg-danger">
-              <CardBody className="pb-0">
-                <ButtonGroup id="card4" className="float-right">
-                  <FiActivity size={'1.5em'} />
-                </ButtonGroup>
-                <div className="text-value">{data.heartrate}</div>
-                <div>Heartrate [bpm]</div>
-              </CardBody>
-              <div className="chart-wrapper" style={{ height: '60px' }}>
-                <HRCard data={data} history={history.miniChart} />
-              </div>
-            </Card>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            <Card>
-              <CardBody>
-                <div
-                  className="chart-wrapper"
-                  style={{ height: `370px`, marginTop: 0 }}
-                >
-                  <MainChart data={data} history={history.chart} />
-                </div>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-
-        <Extra
-          showExtra={true}
-          gear={data.gear}
-          distance={data.distance}
-          time={data.time}
-          weather={weather}
-        />
-      </article>
-    )
+      <Extra
+        showExtra={true}
+        gear={data.gear}
+        distance={data.distance}
+        time={data.time}
+        weather={weather}
+      />
+    </article>
+  )
 }
 
 export default Dashboard
