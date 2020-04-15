a3ea4aff7bae616614ede3c87cf5e2e39a646313
Gabriele <gabriele.belluardo@outlook.it>
2020-04-15 14:12:49 +0200

    Updates

--- src/views/Pages/Page503/Page503test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Page503 from './Page503'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Page503 />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
